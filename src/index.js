709084eb127b07cbac281be8cd056c8b1c7a41d5
1molehayo <segsybro@gmail.com>
2021-03-28 02:50:11 +0100

    complete project setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'assets/sass/main.scss';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
--- src/pages/DefaultPage.js
+++ src/pages/DefaultPage.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { PageLayout } from 'layouts/PageContainer';
 
-const Home = () => {
+const DefaultPage = () => {
   return (
     <PageLayout pageClass="page">
       <section className="section">
         <div className="container">
-          <h3 className="heading">Home</h3>
-          <p className="paragraph">This is a home page</p>
+          <h3 className="heading">Page</h3>
+          <p className="paragraph">This is a generic page</p>
         </div>
       </section>
     </PageLayout>
   );
 };
 
-export default Home;
+export default DefaultPage;
